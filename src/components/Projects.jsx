feb0f3a172ace4507cb9caacfa93f71082e2dc22
kartik428 <kartiksinghpr2003@gmail.com>
2026-02-11 19:57:05 +0530

    Link Updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -142,7 +142,7 @@ export default function ProjectSection() {
       description: "Welcome to My Portfolio website.",
       image: MyPortfolio,
       tags: ["React", "CSS3", "HTML5", "EmailJS", "Googleform"],
-      link: "https://portfolio-xuv1.vercel.app/",
+      link: "https://myportfolio-x.vercel.app/",
     },
     {
       id: 5,
